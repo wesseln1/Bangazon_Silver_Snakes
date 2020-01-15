c14cd37340f59ae0f9a319e5741ca36f70a2dd77
unknown <nwessel429@gmail.com>
2020-01-15 11:16:21 -0600

    added models for product, customer, and computer. Made computer controller as well homie

---
+++ Workforce-Silver-Snakes/Models/Computer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Silver_Snakes.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public DateTime DecomissionDate { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ Workforce-Silver-Snakes/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Silver_Snakes.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Active { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
