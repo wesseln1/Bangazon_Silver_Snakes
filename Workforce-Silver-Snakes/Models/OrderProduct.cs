c345d726be4e65b2b6039c1e5ae1d48d7dd75973
aryn weatherly <arynweatherly93@gmail.com>
2020-01-15 11:28:00 -0600

    made more models

---
+++ Workforce-Silver-Snakes/Models/OrderProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Silver_Snakes.Models
+{
+    public class OrderProduct
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProuductId { get; set; }
+    }
+}
