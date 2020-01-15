5930d0e2203d2bd61afe631b1332b24237c6eaaa
David <dcornish84@gmail.com>
2020-01-15 10:58:55 -0600

    trainingProgram model and controller

---
+++ Workforce-Silver-Snakes/Controllers/TrainingProgramsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Workforce_Silver_Snakes.Controllers
+{
+    public class TrainingProgramsController : Controller
+    {
+        
+
+            private readonly IConfiguration _config;
+
+            public TrainingProgramsController(IConfiguration config)
+            {
+                _config = config;
+            }
+
+            public SqlConnection Connection
+            {
+                get
+                {
+                    return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+                }
+            }
+
+
+
+            // GET: TrainingPrograms
+            public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TrainingPrograms/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TrainingPrograms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingPrograms/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingPrograms/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
