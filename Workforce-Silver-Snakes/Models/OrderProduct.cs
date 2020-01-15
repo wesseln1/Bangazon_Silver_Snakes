49a4314dd224c9b6d6b653d11f6d1d31f1e28491
aryn weatherly <arynweatherly93@gmail.com>
2020-01-15 11:33:41 -0600

    changed prouduct to product

--- Workforce-Silver-Snakes/Models/OrderProduct.cs
+++ Workforce-Silver-Snakes/Models/OrderProduct.cs
@@ -9,6 +9,6 @@ namespace Workforce_Silver_Snakes.Models
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public int ProuductId { get; set; }
+        public int ProductId { get; set; }
     }
 }
