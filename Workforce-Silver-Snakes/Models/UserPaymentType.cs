5bb09ef2809c41d646dcc3fabaa73d7810974197
David <dcornish84@gmail.com>
2020-01-15 11:19:51 -0600

    fixed userpaymenttype model

--- Workforce-Silver-Snakes/Models/UserPaymentType.cs
+++ Workforce-Silver-Snakes/Models/UserPaymentType.cs
@@ -9,6 +9,13 @@ namespace Workforce_Silver_Snakes.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public int AcctNumber { get; set; }
+
+        public bool Active { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int PaymentTypeId { get; set; }
+        
     }
 }
