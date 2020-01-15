e0a83eaf8d7de3beab41ef50a7e24c1c731b9d55
unknown <nwessel429@gmail.com>
2020-01-15 17:11:05 -0600

    able to view and create computers. also view details of each laptop

--- Workforce-Silver-Snakes/Controllers/ComputersController.cs
+++ Workforce-Silver-Snakes/Controllers/ComputersController.cs
@@ -4,37 +4,145 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Workforce_Silver_Snakes.Models;
 
 namespace Workforce_Silver_Snakes.Controllers
 {
     public class ComputersController : Controller
     {
+        private readonly IConfiguration _config;
+        public ComputersController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        } 
         // GET: Computers
         public ActionResult Index()
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Make, Model, DecomissionDate, PurchaseDate
+                                        FROM Computer";
+                    var reader = cmd.ExecuteReader();
+                    var computers = new List<Computer>();
+                    while (reader.Read())
+                    {
+                        computers.Add(new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Model = reader.GetString(reader.GetOrdinal("Model")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate"))
+                        });
+                    }
+                    reader.Close();
+                    return View(computers);
+                }
+            }
         }
 
         // GET: Computers/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id as ComputerId, c.Make, c.Model, c.PurchaseDate, e.Id as EmployeeId, e.FirstName, e.LastName, e.IsSupervisor
+                                     FROM Computer c
+                                     LEFT JOIN Employee e ON e.Id = e.ComputerId
+                                     WHERE c.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    var reader = cmd.ExecuteReader();
+
+                    Computer computer = null;
+
+                    while (reader.Read())
+                    {
+                        if (computer == null)
+                        {
+
+                            computer = new Computer
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                Make = reader.GetString(reader.GetOrdinal("Make")),
+                                Model = reader.GetString(reader.GetOrdinal("Model")),
+                                PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                Employee = new Employee()
+                            };
+                        }
+                        
+                        if (computer.Employee != null)
+                        {
+                            computer.Employee = new Employee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                            };
+                        }
+                    }
+
+                    reader.Close();
+                    if (computer == null)
+                    {
+                        return NotFound();
+                    }
+                    return View(computer);
+                }
+            }
         }
 
         // GET: Computers/Create
         public ActionResult Create()
         {
-            return View();
+            DateTime now = DateTime.Now;
+
+            var computer = new Computer
+            {
+                PurchaseDate = now
+            };
+            return View(computer);
         }
 
         // POST: Computers/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Computer computer)
         {
             try
             {
-                // TODO: Add insert logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Computer 
+                                            (Make, Model, PurchaseDate)
+                                            VALUES (@make, @model, @purchaseDate)";
+
+                        cmd.Parameters.Add(new SqlParameter("@make", computer.Make));
+                        cmd.Parameters.Add(new SqlParameter("@model", computer.Model));
+                        cmd.Parameters.Add(new SqlParameter("@purchaseDate", computer.PurchaseDate));
+
+
+                        //use an excute non query for inserts bc we are not asking for anything back.
+                        //it is a non query- shows that the rows were affected.
+                        cmd.ExecuteNonQuery();
+                    }
+                }
 
                 return RedirectToAction(nameof(Index));
             }
