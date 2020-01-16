3ee0b87a9805c6e7dd6b606e60e63fc49ed7cd7b
David <dcornish84@gmail.com>
2020-01-15 18:32:37 -0600

    details

--- Workforce-Silver-Snakes/Controllers/TrainingProgramsController.cs
+++ Workforce-Silver-Snakes/Controllers/TrainingProgramsController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -12,22 +12,22 @@ namespace Workforce_Silver_Snakes.Controllers
 {
     public class TrainingProgramsController : Controller
     {
-        
 
-            private readonly IConfiguration _config;
 
-            public TrainingProgramsController(IConfiguration config)
+        private readonly IConfiguration _config;
+
+        public TrainingProgramsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
             {
-                _config = config;
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             }
-
-            public SqlConnection Connection
-            {
-                get
-                {
-                    return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-                }
-            }
+        }
 
 
 
@@ -54,7 +54,7 @@ namespace Workforce_Silver_Snakes.Controllers
                             StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
                             EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
                             MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
-                            
+
                         });
                     }
 
@@ -69,7 +69,58 @@ namespace Workforce_Silver_Snakes.Controllers
         // GET: TrainingPrograms/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT tp.Id, tp.Name, tp.StartDate, tp.EndDate, tp.MaxAttendees, 
+                                       e.Id as 'EmployeeId', e.FirstName, e.LastName
+                                       FROM TrainingProgram tp LEFT JOIN EmployeeTraining et ON tp.Id = et.TrainingProgramId
+						               LEFT JOIN Employee e on et.EmployeeId = e.Id
+						               WHERE tp.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    TrainingProgram trainingProgram = null;
+
+                    while (reader.Read())
+                    {
+                        if (trainingProgram == null)
+                        {
+                            trainingProgram = new TrainingProgram
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                                EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                                MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
+                            };
+                        }
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("EmployeeId")))
+                        {
+                            int employeeId = reader.GetInt32(reader.GetOrdinal("EmployeeId"));
+                            if (!trainingProgram.Employees.Any(e => e.Id == employeeId))
+                            {
+                                Employee employee = new Employee
+                                {
+                                    Id = employeeId,
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                    
+                                };
+
+                                trainingProgram.Employees.Add(employee);
+                            }
+                        }
+
+                    }
+
+                    reader.Close();
+                    return View(trainingProgram);
+                }
+            }
         }
 
         // GET: TrainingPrograms/Create
@@ -138,6 +189,35 @@ namespace Workforce_Silver_Snakes.Controllers
             catch
             {
                 return View();
+            }
+        }
+
+        private List<Employee> GetEmployeesById(int employeeId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT e.Id AS EmployeeId, e.FirstName, e.LastName  FROM
+                                        Employee e INNER JOIN EmployeeTraining et ON e.Id = et.EmployeeId
+                                        WHERE EmployeeId = @EmployeeId";
+                    cmd.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    var reader = cmd.ExecuteReader();
+                    List<Employee> employees = new List<Employee>();
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+                        employees.Add(employee);
+                    };
+                    reader.Close();
+                    return employees;
+                }
             }
         }
     }
