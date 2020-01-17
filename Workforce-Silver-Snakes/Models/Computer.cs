7cde53243caf49faeaa7e854a0a5dcf48e14ea1e
Heidi <hspradlin8@gmail.com>
2020-01-17 12:40:32 -0600

    navBar

--- Workforce-Silver-Snakes/Models/Computer.cs
+++ Workforce-Silver-Snakes/Models/Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Workforce_Silver_Snakes.Models
     public class Computer
     {
         public int Id { get; set; }
+        [Display(Name = "Purchase Date")]
         public DateTime PurchaseDate { get; set; }
+        [Display(Name = "Decomission Date")]
         public DateTime? DecomissionDate { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
