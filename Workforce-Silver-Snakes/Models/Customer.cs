1aa9566fc358c46cf3583bb1d2033112aed4e935
Heidi <hspradlin8@gmail.com>
2020-01-17 09:32:19 -0600

    customers

--- Workforce-Silver-Snakes/Models/Customer.cs
+++ Workforce-Silver-Snakes/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,21 @@ namespace Workforce_Silver_Snakes.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
         public string LastName { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool Active { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Email { get; set; }
         public string Phone { get; set; }
     }
